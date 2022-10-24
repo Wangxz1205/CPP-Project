ad2592192396a01a621e6b39cae9404845837e89
11911818@mail.sustech.edu.cn <11911818@mail.sustech.edu.cn>
2022-10-24 15:54:23 +0800

    update .gitignore

---
+++ project2-Calculator/CMakeFiles/a.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wangxz/project2/project2-Calculator/main.cpp" "CMakeFiles/a.dir/main.cpp.o" "gcc" "CMakeFiles/a.dir/main.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/add.cpp" "CMakeFiles/a.dir/src/add.cpp.o" "gcc" "CMakeFiles/a.dir/src/add.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/cal.cpp" "CMakeFiles/a.dir/src/cal.cpp.o" "gcc" "CMakeFiles/a.dir/src/cal.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/cmp.cpp" "CMakeFiles/a.dir/src/cmp.cpp.o" "gcc" "CMakeFiles/a.dir/src/cmp.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/data.cpp" "CMakeFiles/a.dir/src/data.cpp.o" "gcc" "CMakeFiles/a.dir/src/data.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/div.cpp" "CMakeFiles/a.dir/src/div.cpp.o" "gcc" "CMakeFiles/a.dir/src/div.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/judgefuzhi.cpp" "CMakeFiles/a.dir/src/judgefuzhi.cpp.o" "gcc" "CMakeFiles/a.dir/src/judgefuzhi.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/judgeother.cpp" "CMakeFiles/a.dir/src/judgeother.cpp.o" "gcc" "CMakeFiles/a.dir/src/judgeother.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/min.cpp" "CMakeFiles/a.dir/src/min.cpp.o" "gcc" "CMakeFiles/a.dir/src/min.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/mul.cpp" "CMakeFiles/a.dir/src/mul.cpp.o" "gcc" "CMakeFiles/a.dir/src/mul.cpp.o.d"
+  "/home/wangxz/project2/project2-Calculator/src/trans.cpp" "CMakeFiles/a.dir/src/trans.cpp.o" "gcc" "CMakeFiles/a.dir/src/trans.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project3_libraryC/CMakeFiles/a.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wangxz/project/project3_libraryC/main.c" "CMakeFiles/a.dir/main.c.o" "gcc" "CMakeFiles/a.dir/main.c.o.d"
+  "/home/wangxz/project/project3_libraryC/src/func.c" "CMakeFiles/a.dir/src/func.c.o" "gcc" "CMakeFiles/a.dir/src/func.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
