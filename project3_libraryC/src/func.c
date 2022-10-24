94fa1df7c1795c255fb4d9e255ca89ca783dec0a
11911818@mail.sustech.edu.cn <11911818@mail.sustech.edu.cn>
2022-10-24 23:52:10 +0800

    update

--- project3_libraryC/src/func.c
+++ project3_libraryC/src/func.c
@@ -2,6 +2,8 @@
 
 matrix *createMatrix(int row, int column, float *data)
 {
+    assert(row > 0 && column > 0);
+    
     matrix *m = (matrix *)malloc(sizeof(matrix));
     m->row = row;
     m->column = column;
