ac103eeacdc8d5556d7668a5829ee0e752c1c398
Wangxz1205 <11911818@mail.sustech.edu.cn>
2022-11-29 14:36:10 +0800

    update

---
+++ project5_Class_for_Matrices/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Matrix
+{
+private:
+    size_t row;
+    size_t column;
+    float * pdata;
+    size_t channel;
+public:
+    Matrix(/* args */);
+    ~Matrix();
+};
+
+Matrix::Matrix(/* args */)
+{
+}
+
+Matrix::~Matrix()
+{
+}
